dd70cdacbabed25c9c848c3dd4c6a91935cf3519
Bright Selva Kumaran <110169833+BrightSelvaKumaran@users.noreply.github.com>
2023-08-21 12:09:40 +0530

    Update dataone.js with new data record

--- script/data/dataone.js
+++ script/data/dataone.js
@@ -1,5 +1,14 @@
 var linkName = [{
-    link:"https://www.google.com"
+    link:"https://www.crypmon.com/2023/08/defi-unlocked-acquit-financial.html"
 },{
-    link:"https://www.google.com"
-}]
+    link:"https://www.crypmon.com/2023/08/navigating-new-era-web-30s-impact-on-e.html"
+},
+{
+    link:"https://www.crypmon.com/2023/08/harnessing-power-of-distributed-ledgers.html"
+},
+{
+    link:"https://www.crypmon.com/2023/07/altcoins-unleashed-uncovering.html"
+},
+{
+    link:"https://www.crypmon.com/2023/07/monetizing-digital-revolution-roadmap.html"
+},]
