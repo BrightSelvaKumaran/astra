3599fb7a4096afccf23faf557127cfe233196c94
Bright Selva Kumaran <110169833+BrightSelvaKumaran@users.noreply.github.com>
2023-09-07 09:46:56 +0530

    Update dataone.js data with added data

--- script/data/dataone.js
+++ script/data/dataone.js
@@ -1,26 +1,36 @@
-var linkName = [{
-    link:"https://www.crypmon.com/2023/08/defi-unlocked-acquit-financial.html"
-},{
-    link:"https://www.crypmon.com/2023/08/navigating-new-era-web-30s-impact-on-e.html"
-},
-{
-    link:"https://www.crypmon.com/2023/08/harnessing-power-of-distributed-ledgers.html"
-},
-{
-    link:"https://www.crypmon.com/2023/07/altcoins-unleashed-uncovering.html"
-},
-{
-    link:"https://www.crypmon.com/2023/07/monetizing-digital-revolution-roadmap.html"
-},
-{
-    link:"https://www.crypmon.com/2023/07/unearthing-clouds-gold-examining-return.html"
-},
-{
-    link:"https://www.crypmon.com/2023/07/crypto-empowerment-8-surefire-ways-to.html"
-},
-{
-    link:"https://www.crypmon.com/2023/07/investment-revolution-unlocking-new_10.html"
-},
-{
-    link:"https://www.crypmon.com/2023/07/from-ethereum-to-hyperledger-analyzing_10.html"
-}]
+ var linkName = [{
+      link:"https://www.crypmon.com/2023/08/defi-unlocked-acquit-financial.html"
+  },{
+      link:"https://www.crypmon.com/2023/08/navigating-new-era-web-30s-impact-on-e.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/08/harnessing-power-of-distributed-ledgers.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/07/altcoins-unleashed-uncovering.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/07/monetizing-digital-revolution-roadmap.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/07/unearthing-clouds-gold-examining-return.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/07/crypto-empowerment-8-surefire-ways-to.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/07/investment-revolution-unlocking-new_10.html"
+  },
+  {
+      link:"https://www.crypmon.com/2023/07/from-ethereum-to-hyperledger-analyzing_10.html"
+  },
+  {
+    link:"https://www.crypmon.com/2023/09/cracking-code-unraveling-inner-workings.html"
+  },  
+  {
+    link:"https://www.crypmon.com/2023/09/asset-tokenization-building-fortified.html"
+  },
+  {
+    link:"https://www.crypmon.com/2023/09/cloud-mining-in-blockchain-risky-gamble.html"
+  },
+]
